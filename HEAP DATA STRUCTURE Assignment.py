bca1b4b273c1e1e11dc17ef4a590956fd347f591
Narender Kumar <narenderbeniwal1234@gmail.com>
2022-03-17 08:44:10 +0530

    Add files via upload

---
+++ HEAP DATA STRUCTURE Assignment.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+# coding: utf-8
+1. Describe Python's built-in data structure?
+2. Describe the Python user data structure?
+3. Describe the stages involved in writing an algorithm?
+4. Outline the components of a good algorithm?
+5. Describe the Tree traversal method?
+
+6. Explain the difference between inorder and postorder tree traversal?Q. 1. Describe Python's built-in data structure?
+Sol:
+    Data structures are basically just that they are structures which can hold some data together. In
+other words, they are used to store a collection of related data. There are four built-in data 
+structures in Python-list, tuple, dictionary and set.Q. 2. Describe the Python user data structure?
+Sol:Python supports many user-defined data structures i.e. the user can create their own data structures, like Stack, Queue, Tree, Linked List, Graph, and Hash Map.
+
+Stack
+
+Stacks are linear Data Structures which are based on the principle of Last-In-First-Out (LIFO) where data which is entered last will be the first to get accessed.
+
+Queue
+
+A queue is also a linear data structure which is based on the principle of First-In-First-Out (FIFO) where the data entered first will be accessed first.
+
+Linked List
+
+Linked lists are linear Data Structures which are not stored consequently but are linked with each other using pointers. The node of a linked list is composed of data and a pointer called next.
+
+Tree
+
+A tree is a non-linear data structure consisting of roots and nodes. The topmost base node is the root, and the elements present at the end of the tree are leaves. 
+
+Graph
+
+A graph is a non-linear data structure consisting of nodes and edges. Nodes are known as vertices, and the lines connecting two nodes are generally called nodes.
+
+HashMap
+
+Hash Maps are indexed data structures and perform the same function as that performed by dictionary in Python. A hash map uses a hash function to compute index-key values into arrays. 
+
+Q. 3. Describe the stages involved in writing an algorithm?
+Sol : Algorithm is a step by step set of instructions designed to perform a specific task.
+
+Steps involved in writing an algorithm are as below:
+
+Obtain detailed information on the problem.
+Analyze the problem.
+Think of a problem solving approach.
+Review the problem solving approach and try to think of a better Alternative.
+Develop a basic structure of the Algorithm.
+Optimize, Improvise and refine.
+Review the algorithm.Q. 4. Outline the components of a good algorithm?
+
+Sol: Below are the main components of a good algorithm
+    
+1. Unambiguous − Algorithm should be clear.
+2. Input − An algorithm should have 0 or more well-defined inputs.
+3. Output − An algorithm should have 1 or more well-defined outputs, and should match the desired output.
+4. Finiteness − Algorithms must terminate after a finite number of steps.
+5. Well-Ordered - The exact order of operations performed in an algorithm should be concretely defined.
+6. Independent - An algorithm should have step-by-step directions, which should be independent of any programming code.Q. 5. Describe the Tree traversal method?
+Sol: Tree traversal involves traversing each node of the tree at least once where the information in the form of key and value can be retrieved for easy identification.
+
+
+Q. 6. Explain the difference between inorder and postorder tree traversal?
+
+Sol :INORDER TRAVERSAL -
+
+An inorder traversal is a traversal technique that follows the policy, i.e., Left -> Root -> Right.
+
+Here, the left subtree of the root node is traversed first, then the root node, and then the right subtree of the root node is traversed.
+
+POSTORDER TRAVERSAL -
+
+A Postorder traversal is a traversal technique that follows the policy, i.e., Left -> Right -> Root.
+
+Here, the left subtree of the root node is traversed first, then the right-side sub-tree, and then finally the node is traversed.
